6ed91195a1feeb729c801c2083fa40f260dc0007
UserRic <enrico.dbulatao@gmail.com>
2025-08-24 21:02:18 +0800

    updated Models user and migrations folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin',
+        'is_blocked',
     ];
 
     /**
@@ -43,6 +45,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
+            'is_blocked' => 'boolean',
         ];
     }
 }
