a2318d41f2d90b98f2788bac8648c430ef1b618d
cmspastores <cmspastores@addu.edu.ph>
2025-09-05 13:48:17 +0800

    Changees on index

--- app/Http/Controllers/LoginLogController.php
+++ app/Http/Controllers/LoginLogController.php
@@ -10,6 +10,6 @@ class LoginLogController extends Controller
     public function index()
     {
         $logs = LoginLog::latest()->paginate(10); // paginated list
-        return view('login_logs.index', compact('logs'));
+        return view('index', compact('logs'));
     }
 }
